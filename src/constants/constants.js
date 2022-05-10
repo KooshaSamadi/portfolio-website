edaa70404e28c7b1311e15cda6330f69ed567de3
unknown <kooshasamadi@gmail.com>
2022-05-10 09:30:01 +0430

    constant changed

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -2,7 +2,8 @@ export const projects = [
   {
     title: "Whatspp",
     description: "This was my final project for university",
-    image: "/images/1.png",
+    image:
+      "https://user-images.githubusercontent.com/42487233/167545324-c74303b3-4462-4a2f-a61b-4684996acf99.PNG",
     tags: ["React", "Html", "Scss", "Javascript", "Firebase"],
     source: "https://github.com/KooshaSamadi/whatsapp-clone-final",
     visit: "https://whatsapp-clone-koosha.netlify.app/",
@@ -12,7 +13,8 @@ export const projects = [
     title: "People Rolodex ",
     description:
       "َA very simple react application for searching and filtering people ",
-    image: "/images/2.png",
+    image:
+      "https://user-images.githubusercontent.com/42487233/167545461-683caaf1-594a-4c1b-bb0a-8113721ef4f3.PNG",
     tags: ["React", "Html", "Scss", "Javascript", "Api"],
     source: "https://github.com/KooshaSamadi/People-Rolodex",
 
@@ -21,7 +23,8 @@ export const projects = [
   {
     title: "Beatmaker",
     description: "This Webapp is for playing music and making one",
-    image: "/images/3.png",
+    image:
+      "https://user-images.githubusercontent.com/42487233/167545668-080fcc6a-18e2-4229-a467-18e52e0ccc65.PNG",
     tags: [, "Html", "Scss", "Advanc Javascript"],
     source: "https://github.com/KooshaSamadi/Beatmaker",
 
@@ -30,7 +33,8 @@ export const projects = [
   {
     title: "Wave music app",
     description: "A website for playing music. It fetches music from an Api",
-    image: "/images/4.png",
+    image:
+      "https://user-images.githubusercontent.com/42487233/167543117-53ba5562-5afc-477a-9ca7-587a9c3902e2.PNG",
     tags: ["React", "Html", "Scss", "Javascript"],
     source: "https://github.com/KooshaSamadi/React-Waves-Music-App",
     visit: "https://dreaminatemusics.netlify.app/",
@@ -39,7 +43,8 @@ export const projects = [
   {
     title: "Ignite Game Webapp",
     description: "A website for filtering and searching games",
-    image: "/images/5.png",
+    image:
+      "https://user-images.githubusercontent.com/42487233/167545593-589cb104-ff04-4b25-9ae7-d9bb214e3653.PNG",
     tags: ["React", "Html", "Scss", "Javascript"],
     source: "https://github.com/KooshaSamadi/react-redux-ignite",
 
